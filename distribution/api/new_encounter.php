ac35d5832975b2be5284c2384032957cd5daa9ae
Matthew <thoughtfulmonkey@gmail.com>
2025-04-04 20:42:44 +0100

    Install and documentation updates for global configs

--- distribution/api/new_encounter.php
+++ distribution/api/new_encounter.php
@@ -8,7 +8,7 @@ require './bumpcheck.php';
 $debugReturn = '';
 
 // Version number for encounter json
-$version = "en-0.1";
+$version = "en-0.2";
 
 // https://stackoverflow.com/questions/6041741/fastest-way-to-check-if-a-string-is-json-in-php
 function isJson($string) {
